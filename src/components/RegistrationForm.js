d3ebca7908cfe9a6096963614d63cb4a2ba2d664
Paulo Azoia <pazoia.gomes@gmail.com>
2022-02-03 15:01:41 +0000

    changed file name to better describe its functionality

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import "../css/RegistrationForm.css"
+
+class RegistrationForm extends React.Component {
+    nameRef = React.createRef();
+    surnameRef = React.createRef();
+    emailRef = React.createRef();
+    passwordRef = React.createRef();
+
+    createMember = (event) => {
+        event.preventDefault();
+
+        const newMember = {
+            name: this.nameRef.current.value,
+            surname: this.surnameRef.current.value,
+            email: this.emailRef.current.value,
+            password: this.passwordRef.current.value,
+        }
+
+        this.props.addMember(newMember);
+
+        console.log(newMember);
+        event.currentTarget.reset();
+    };
+
+    render() {
+        return (                
+            <div className="register">
+                <form onSubmit={this.createMember}>
+                    <h1>Registration Form</h1>
+                    <input name="name" ref={this.nameRef} type="text" placeholder="Name" required />
+                    <input name="surname" ref={this.surnameRef} type="text" placeholder="Surname" required />
+                    <input name="email" ref={this.emailRef} type="email" placeholder="Email" required />
+                    <input name="password" ref={this.passwordRef} type="password" placeholder="Password" required />
+                    <button type="submit">Register</button>
+                </form>
+            </div>                   
+        );
+    }
+}
+
+export default RegistrationForm;
