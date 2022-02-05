3c1e0b481ceff9b882e5aed87c90d87e24cbc00a
Paulo Azoia <pazoia.gomes@gmail.com>
2022-02-05 23:57:12 +0000

    added About us and Blog components, currently only displaying titles

---
+++ src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "../css/AboutUs.css"
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Us</h1>
+    </div>
+  );
+}
+
+export default AboutUs;
