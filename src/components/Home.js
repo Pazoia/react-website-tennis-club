f599d8cd390414aa2421c6bd7cf2eef19d106164
Paulo Azoia <pazoia.gomes@gmail.com>
2022-02-06 00:01:03 +0000

    added Home component, with title and button to join club

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Button from './Button';
+import "../css/Home.css"
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>GOLDEN SANDS</h1>
+      <p>Playing tennis by the beach!</p>
+      <div className="join-us-btn">
+        <Button
+          link="/register"
+          className="btn"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Join Us Today
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
