52a12d5b9e2a9063b043320839e717a016bf8b42
Paulo Azoia <pazoia.gomes@gmail.com>
2022-02-07 11:38:39 +0000

    refactored sorting functions to work with object properties

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -1,6 +1,6 @@
 export function sortByPoints(members) {
   const objArray = Object.entries(members);
-  objArray.sort((a, b) => Object.entries(b[1])[2][1] - Object.entries(a[1])[2][1]);
+  objArray.sort((a, b) => b[1].points - a[1].points);
   const backToObj = Object.fromEntries(objArray);
   
   return backToObj;
@@ -9,23 +9,22 @@ export function sortByPoints(members) {
 export function sortByName(members) {
   const objArray = Object.entries(members);
   objArray.sort((a, b) => {
-    if (Object.entries(b[1])[0][1] > Object.entries(a[1])[0][1]) {return -1;}
-    if (Object.entries(b[1])[0][1] < Object.entries(a[1])[0][1]) {return 1;}
+    if (b[1].name > a[1].name) {return -1;}
+    if (b[1].name < a[1].name) {return 1;}
     return 0;
   });
   const backToObj = Object.fromEntries(objArray);
-  
   return backToObj;
 };
 
 export function sortBySurname(members) {
   const objArray = Object.entries(members);
   objArray.sort((a, b) => {
-    if (Object.entries(b[1])[1][1] > Object.entries(a[1])[1][1]) {return -1;}
-    if (Object.entries(b[1])[1][1] < Object.entries(a[1])[1][1]) {return 1;}
+    if (b[1].surname > a[1].surname) {return -1;}
+    if (b[1].surname < a[1].surname) {return 1;}
     return 0;
   });
   const backToObj = Object.fromEntries(objArray);
-  
+
   return backToObj;
 };
